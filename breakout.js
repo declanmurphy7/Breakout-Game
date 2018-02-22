5ca973654bcd9e62a096673d95628194d1834884
declanmurphy7 <35809332+declanmurphy7@users.noreply.github.com>
2018-02-22 16:55:13 +0000

    V6 Define Paddle

--- breakout.js
+++ breakout.js
@@ -6,6 +6,11 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
 
 function drawBall() {
@@ -13,12 +18,22 @@ function drawBall() {
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
-	ctx.closePath();	
+	ctx.closePath();
+		
 }
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
 
 function draw() {
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 	drawBall();
+	drawPaddle();
 	x += dx;
 	y += dy;
 
@@ -40,6 +55,9 @@ if(x + dx>canvas.width-ballRadius || x + dx <ballRadius){
 }
 
 }
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup" , keyUpHandler, false);
+
 
 
 setInterval(draw, 10);
