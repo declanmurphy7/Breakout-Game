f49266e2b30a24631480cb926a5827324b90aa9a
declanmurphy7 <35809332+declanmurphy7@users.noreply.github.com>
2018-02-22 16:23:28 +0000

    V5 Bounce off the Walls

--- breakout.js
+++ breakout.js
@@ -5,7 +5,7 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-var ballRadius = 13;
+var ballRadius = 10;
 
 
 function drawBall() {
@@ -13,18 +13,34 @@ function drawBall() {
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
-	ctx.closePath();
-	
-	
+	ctx.closePath();	
 }
+
 function draw() {
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 	drawBall();
 	x += dx;
 	y += dy;
-	
-	
+
+
+if(y + dy < ballRadius){
+	dy = -dy;
 }
+
+if(y + dy>canvas.height-ballRadius){
+	dy = -dy;
+}
+
+if(y + dy>canvas.height-ballRadius || y + dy <ballRadius){
+	dy = -dy;
+}
+
+if(x + dx>canvas.width-ballRadius || x + dx <ballRadius){
+	dx = -dx;
+}
+
+}
+
 
 setInterval(draw, 10);
 
